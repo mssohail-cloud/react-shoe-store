9a5ee0edbf23738f62b1ca39cbfe749df0581cea
Muhammad Sohail <muhammadsohail87@gmail.com>
2020-09-13 11:15:55 +0500

    products component completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import Products from './components/Products';
+import data from './data.json';
 
 
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React shopping cart</a>
-      </header>
-      <main>
-        Product List
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      products: data.products, 
+      size: "",
+      sort: ""
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+              Cart Items
+
+            </div>
+          </div>
       </main>
-      <footer>
-        All rights reserved
+        <footer>
+          All rights reserved
       </footer>
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
 export default App;
