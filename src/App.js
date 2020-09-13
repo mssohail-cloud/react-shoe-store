ba3a056d1aac33b48b5f678c58e545e96c687dda
Muhammad Sohail <muhammadsohail87@gmail.com>
2020-09-13 14:36:44 +0500

    filter component completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json';
 
@@ -6,14 +7,47 @@ import data from './data.json';
 
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      products: data.products, 
+      products: data.products,
       size: "",
       sort: ""
+      
 
     }
+    //console.log(this.state.products)
+  }
+
+  sortProducts = (event) => {
+
+    const sort = event.target.value;
+
+    console.log(event.target.value);
+
+    this.setState((state) => ({
+      sort: sort, 
+      products: this.state.products.slice().sort((a,b) => (
+        sort === "lowest"? ((a.price > b.price) ? 1:-1):
+        sort === "highest" ? ((a.price < b.price) ? 1:-1):
+      ((a._productId < b._productId)? 1:-1)
+      ))
+    }))
+  }
+
+  filterProducts = (event) => {
+
+   // console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products })
+    }
+    else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter((product) => product.availableSizes.indexOf(event.target.value) >= 0)
+      })
+    }
+
   }
 
   render() {
@@ -25,6 +59,11 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts} />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">
@@ -32,7 +71,7 @@ class App extends React.Component {
 
             </div>
           </div>
-      </main>
+        </main>
         <footer>
           All rights reserved
       </footer>
